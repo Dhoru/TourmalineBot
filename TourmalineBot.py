44c22e7b43d0734159afb98d514682658e459e0a
Dhoru <70144216+Dhoru@users.noreply.github.com>
2020-11-30 10:03:25 +0500

    Update TourmalineBot.py

--- TourmalineBot.py
+++ TourmalineBot.py
@@ -1,98 +1,75 @@
+import praw
 import discord
 from discord.ext import commands
 import time
 import random
 import os
+from catfacts import catfacts
+import platform
+import datetime
+import json
 bot = commands.Bot(command_prefix='!!')
-@bot.remove_command('help')
+bot.remove_command('help')
+
+reddit = praw.Reddit(client_id='MRtY6ttHixEa0A',
+                     client_secret='D6sKN7YJ84ZcBRBFjM1Up-dQLlk',
+                     user_agent='tourmalinebot by u/Dhoru_')  
 
 @bot.command()
 async def info(ctx):
-  embed = discord.Embed(title='TourmalineBot - Info')
-  embed.add_field(name="This bot was made using discord.py by dhoru#7700", value="Join the server at https://discord.gg/cthA5Pv !", inline=False)
+    serverCount = len(bot.guilds)
+    memberCount = len(set(bot.get_all_members()))
 
-  await ctx.send(embed=embed)
+    embed = discord.Embed(title=f'{self.bot.user.name} Stats', description='\uFEFF', colour=ctx.author.colour, timestamp=ctx.message.created_at)
 
+    embed.add_field(name='Bot Version:', value=self.bot.version)
+    embed.add_field(name='Python Version:', value=pythonVersion)
+    embed.add_field(name='Discord.Py Version', value=dpyVersion)
+    embed.add_field(name='Total Guilds:', value=serverCount)
+    embed.add_field(name='Total Users:', value=memberCount)
+    embed.add_field(name='Made By:', value="dhoru#7700")
+
+    embed.set_footer(text=f"Carpe Noctem | {self.bot.user.name}")
+    embed.set_author(name=bot.user.name, icon_url=self.bot.user.avatar_url)
+
+    await ctx.send(embed=embed) 
+  
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title='TourmalineBot - Main Commands')
-    embed.add_field(name="!!info", value="shows bot info.", inline=True)
-    embed.add_field(name="!!info", value="shows bot info.", inline=True)
-    embed.add_field(name="!!say <message>", value="bot repeats what you say.", inline=True)
-    embed.add_field(name="!!react <emoji>", value="""reacts to your message
-          with the specified emoji.""", inline=True)
-    embed.add_field(name="!!ping", value="shows bot latency.", inline=True)
-    embed.set_footer(text="Hello World")
+    embed = discord.Embed(title="Tourmaline - Commands", description="""
+    **Main commands:**
+    `!!info` - Shows bot info (under development)
+    `!!say <message>` - Bot repeats what you say.
+    `!!react <emoji>` - Reacts to your message with the specified emoji.
+    `!!ping` - Shows bot latency.
+    `!!invite` - Link to invite the bot.
+
+    **Fun commands:**
+    `!!coinflip` - Flips a coin.
+    `!!meme` - Memes fresh from r/dankmemes.
+    `!!catfact` Random cat fact.
+
+    **Utility/Moderation commands:**
+    `!!setnick <@user>` - Changes the specified user's nickname.
+    `!!roll <number of dice> <number of sides in each dice>` - Simulates rolling dice.
+    """)
+    embed.set_footer(text="TourmalineBot - Made by dhoru#7700")
     embed.colour = 0xFFFFFF  # can be set in 'discord.Embed()' too
 
     await ctx.send(embed=embed)
-
-    embed = discord.Embed(title='TourmalineBot - Fun commands:')
-    embed.add_field(name="!!coinflip", value="flips a coin.", inline=True)
-    embed.add_field(name="!!point", value="ascii finger point", inline=True)
-    embed.add_field(name="!!clouds", value="ascii clouds", inline=True)
-    embed.colour = 0xFFFFFF 
-
-    await ctx.send(embed=embed)
-
   # or: await destination.send(embed=embed)
 
 #឵឵឵឵឵឵
-@bot.command()
-async def point(ctx):
-  embed = discord.Embed(title='឵឵')
-  embed.add_field(name="ASCII hand sign", value="""
-  ```fix
-      mmm_
-       YMMmm._
-         "MMMMMm.
-           '"YMMMbm.
-       _,mmmm._mMMMMMm.
-    ,mMMMMYMMMMMMMMMPMMMm.
-   dMMMMP   '"YMMMMMUMMMLM.
-  dMMMP'        `YMMPYMPM8Mb_
-mdMMM_             MM8mMMmM_Yb.
-YMMMMMMm___     ,mMPdMMMMMMMMmYM.
-   YPM8MMMM8mmmmmmmMMMMMMMMMMMMbM.
-      '`Y8MMMMMMMMMMMMMMMMMMMMMMMM._
-           `YM8PMMMMMMMMMMMMMMMMMMMMm__
-              `"MbYMMMMMMMMMMMMMMMMMMMMbL_
-                 'YYMPPMMMMMMMMMMMMMMMMMMMMm_
-                     `"PM"YMMMMMMMMMMMMMMmMMb.
-                         `"YMMMMMMMMMMMMMMMbMMM_
-                             YMMMMMMMMMMMMMMMMMM
-                               YMMMMMMMMMMMMMMMM
-  -Dhoru#7700-                  YMMMMMMMMMMMMMMM
-                                  `YMMMMMMMMMMMP
-                                    `YMMMMMMMMMM
-                                      `MMMMMMMMM
-                                       `MMMMMdMM
-```""", inline=False)
-  await ctx.send(embed=embed)
-
-
-@bot.command()
-async def clouds(ctx):
-    await ctx.send('''
-```fix
-                     _________
-     ___            (         )
-    (   )__       _(           )___
-   (_______)...  (_________________)
-
-```
-     ''')
-
 @bot.command()
 async def react(ctx, arg):
     await ctx.message.add_reaction(arg)
 
 @bot.command()
 async def say(ctx, *, arg):
-    if arg == "@everyone":
-        await ctx.send("no mass mentioning.")
-    elif arg == "@here":
-        await ctx.send("no mass mentioning.")
+    message_components = arg.split()
+    if "@everyone" in message_components or "@here" in message_components:
+        await ctx.send("You cannot have ``@everyone`` or ``@here`` in your message!")
+        await ctx.message.delete()
     else:
         await ctx.send(arg)
         await ctx.message.delete()
@@ -104,40 +81,71 @@ async def coinflip(ctx):
         "tails",]
     await ctx.send(random.choice(coin))
 
-@bot.command(pass_context=True)
-async def kill(ctx, arg):
-    murder_message = [
-    "dies of covid-19"
-    ]
-    await ctx.send("{}".format(arg, random.choice(murder_message)))
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong! `{0} ms`'.format(round(bot.latency, 1)))
+
+@bot.command()
+async def invite(ctx):
+	embed=discord.Embed(title="Invite Link", url="https://discord.com/api/oauth2/authorize?client_id=749910944951435264&permissions=8&scope=bot")
+	embed.colour = 0xFFFFFF 
+	
+	await ctx.send(embed=embed)
+
+
+@bot.command()
+async def ohnoanyway(ctx):
+  await ctx.send("https://i.kym-cdn.com/photos/images/original/001/883/586/63f.jpg")
+
+@bot.command()
+async def catfact(ctx):
+    await ctx.send(random.choice(catfacts))
 
 @commands.is_owner()
 @bot.command()
 async def tr(ctx):
   await ctx.send("https://discord.com/developers/applications/749910944951435264/bot")
 
-@bot.event
-async def on_ready():
-    activity = discord.Game(name="add status here", type=3)
-    await bot.change_presence(status=discord.Status.online, activity=activity)
-    print("Bot is ready!")
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+@bot.command(pass_context=True)
+async def setnick(ctx, member: discord.Member, nick):
+    await member.edit(nick=nick)
+    await ctx.send(f'Nickname was changed for {member.mention} ')
 
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
+@bot.command()
+async def tourmaline_rules(ctx):
+    embed=discord.Embed(title="Rules:", description="<a:ruby:763440863027396628> Use common sense pls \n \n<a:ruby:763440863027396628> No mass mentioning of any form or spamming of any sort\n\n<a:ruby:763440863027396628> Do not annoy, dissolve, digest, or threaten anyone.\n \n <a:ruby:763440863027396628> No NSFW content.\n \n <a:ruby:763440863027396628> Don't abuse loopholes. \n \n<a:ruby:763440863027396628> ***Either*** your username *or* nickname should be pingable.\n \n <a:ruby:763440863027396628> No advertising servers (includes DM advertising). In case it does happen anyway, Report it to a staff member. (apply for partnership #┃partners)\n\n<a:ruby:763440863027396628> Do not DM staff about mutes/ warns.\n\n<a:ruby:763440863027396628> Alt accounts are **NOT** allowed.\n\n<a:ruby:763440863027396628> No begging for roles.\n\n<a:ruby:763440863027396628> No being an imbecile.")
+    embed.colour = 0x00FF00
+    await ctx.send(embed=embed)
+    await ctx.message.delete()
+
+@bot.command()
+async def meme(ctx):
+    memes_submissions = reddit.subreddit('dankmemes').hot()
+    post_to_pick = random.randint(1, 50)
+    for i in range(0, post_to_pick):
+        submission = next(x for x in memes_submissions if not x.stickied)
+
+    embed=discord.Embed(title=submission.title, url=submission.url, color=0x0af0d5)
+    embed.set_image(url=submission.url)
+    await ctx.send(embed=embed)
+
+@bot.command()
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+    
+@bot.event
+async def on_ready():
+    activity = discord.Game(name="Dhoru is Epic | !!help", type=3)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+    print("------")
+    print("Bot is ready!")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
-
-
-bot.run('insert your token here')
+bot.run(token)
